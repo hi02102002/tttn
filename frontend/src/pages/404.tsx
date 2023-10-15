9d111138e6d22f51ff941b18b1109b5d30f058b8
Võ Hoàng Huy <hoanghuy.dev0210@gmail.com>
2023-10-15 15:55:13 +0700

    update page not found

--- frontend/src/pages/404.tsx
+++ frontend/src/pages/404.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import { Button } from '@/components/ui';
+import { ROUTES } from '@/constants';
+import { useUser } from '@/contexts/user.ctx';
+import { TUser } from '@/types/user';
+import { isAdmin } from '@/utils';
+import { useRouter } from 'next/router';
 
-type Props = {};
+const NotFound = () => {
+   const { user } = useUser();
+   const router = useRouter();
 
-const NotFound = (props: Props) => {
-   return <div>NotFound</div>;
+   return (
+      <div className="min-h-screen p-4 flex items-center justify-center">
+         <div className="space-y-2 flex flex-col items-center">
+            <h2 className="text-6xl font-bold text-center">404</h2>
+            <p className="text-xl font-medium text-center text-muted-foreground">
+               Page not found
+            </p>
+            <Button
+               onClick={() => {
+                  router.push(
+                     isAdmin(user as TUser)
+                        ? ROUTES.ADMIN
+                        : ROUTES.STUDENT_DASHBOARD
+                  );
+               }}
+            >
+               Back to home
+            </Button>
+         </div>
+      </div>
+   );
 };
 
 export default NotFound;
